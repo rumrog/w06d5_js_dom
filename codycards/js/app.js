75c78c25571f1adea2c88a104d2794feb075ad96
Rumen Roger <rumenroger@gmail.com>
2020-06-21 22:42:01 +0100

    MVP.4: append the submitted data to the list

--- codycards/js/app.js
+++ codycards/js/app.js
@@ -7,8 +7,27 @@ const handleNewItemFormSubmit = event => {
   event.preventDefault();
 
   const flashcardItem = createFlashcardItem(event.target);
-  const flashcard = document.querySelector('#flashcard');
+  const flashcard = document.querySelector('#flashcards');
   flashcard.appendChild(flashcardItem);
 
   event.target.reset();
+}
+
+const createFlashcardItem = form => {
+  const flashcardItem = document.createElement('li');
+  flashcardItem.classList.add('flashcards-list-item');
+
+  const language = document.createElement('p');
+  language.textContent = form.language.value;
+  flashcardItem.appendChild(language);
+
+  const topic = document.createElement('h2');
+  topic.textContent = form.topic.value;
+  flashcardItem.appendChild(topic);
+
+  const concept = document.createElement('h3');
+  concept.textContent = form.concept.value;
+  flashcardItem.appendChild(concept);
+
+  return flashcardItem;
 }
